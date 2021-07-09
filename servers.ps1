3da0af4794a928fe15fe721b466c926a5669e8c5
SyntaxBearror <53105897+syntaxbearror@users.noreply.github.com>
2021-07-09 16:46:29 -0500

    Add files via upload

---
+++ servers.ps1
@@ -0,0 +1,41 @@
+﻿# -----------------------------------------------------------------
+# Set Registry Keys for PrintNightmare [CVE-2021-1675] - For Servers
+# Created by: Christopher Clai (syntaxbearror.io)
+# -----------------------------------------------------------------
+# Version 1.0 (July 9th, 2021)
+# -----------------------------------------------------------------
+#
+# Use this on systems as part of the final mitigation to the CVE-2021-1675 exploit.
+#
+# Example of running the script:
+# .\servers.ps1
+#
+#
+# ##### CHANGELOG ########
+# Version 1.0
+# - It is born!
+#
+#
+
+# Registry Changes for PrintNightmare for Servers
+Write-Host "Creating Keys and Values..."
+
+# Create the keys if not present
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers" -ErrorAction SilentlyContinue
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" -ErrorAction SilentlyContinue
+
+# Create / Set the values
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" -Name NoWarningNoElevationOnInstall -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" -Name UpdatePromptSettings -Value 0
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" -Name RestrictDriverInstallationToAdministrators -Value 1
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name EnableLUA -Value 1
+
+# Output Results
+Write-Host "Status of Keys..."
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" -Name NoWarningNoElevationOnInstall
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" -Name UpdatePromptSettings
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint" -Name RestrictDriverInstallationToAdministrators
+
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name EnableLUA
+Write-Host "Completed."
---
+++ workstations.ps1
@@ -0,0 +1,32 @@
+﻿# -----------------------------------------------------------------
+# Set Registry Keys for PrintNightmare [CVE-2021-1675] - For Workstations
+# Created by: Christopher Clai (syntaxbearror.io)
+# -----------------------------------------------------------------
+# Version 1.0 (July 9th, 2021)
+# -----------------------------------------------------------------
+#
+# Use this on systems that shouldn't host shared printers which will block RPC connections.
+#
+# Example of running the script:
+# .\workstations.ps1
+#
+#
+# ##### CHANGELOG ########
+# Version 1.0
+# - It is born!
+#
+#
+
+# Registry Changes for PrintNightmare for Servers
+Write-Host "Creating Keys and Values..."
+
+# Create the keys if not present
+New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers" -ErrorAction SilentlyContinue
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers" -Name RegisterSpoolerRemoteRPCEndPoint -Value 2
+
+# Output Results
+Write-Host "Status of Keys..."
+Get-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers" -Name RegisterSpoolerRemoteRPCEndPoint
+
+Write-Host "Completed."
